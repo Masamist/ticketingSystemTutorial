d9fbd2b6e47cc94e753ac6852303498c21901b8a
Masami <msm-k0304@hotmail.com>
2024-05-05 07:45:40 +1200

    add sorting function

--- app/tickets/DataTable.tsx
+++ app/tickets/DataTable.tsx
@@ -3,12 +3,15 @@ import TicketStatusBadge from "@/components/TicketStatusBadge"
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table"
 import { Ticket } from "@prisma/client"
 import Link from "next/link"
+import { ArrowDown } from "lucide-react"
+import { SearchParams } from "./page"
 
 interface Props {
   tickets: Ticket[]
+  searchParams: SearchParams
 }
 
-const DataTable = ({tickets}: Props) => {
+const DataTable = ({tickets, searchParams}: Props) => {
   return (
     <div className="w-full mt-5">
       <div className="rounded-md sm:border">
@@ -16,20 +19,24 @@ const DataTable = ({tickets}: Props) => {
           <TableHeader>
             <TableRow>
               <TableHead>
-                Title
+                <Link href={{query: {...searchParams, orderBy: "title"}}}>Title</Link>
+                {"title" === searchParams.orderBy && <ArrowDown className="inline p-1" />}
               </TableHead>
               <TableHead>
                 <div className="flex justify-center">
-                  Status
+                <Link href={{query: {...searchParams, orderBy: "status"}}}>Status</Link>
+                {"status" === searchParams.orderBy && <ArrowDown className="inline p-1" />}
                 </div>
               </TableHead>
               <TableHead>
                 <div className="flex justify-center">
-                  Priority
+                <Link href={{query: {...searchParams, orderBy: "priority"}}}>Priority</Link>
+                {"priority" === searchParams.orderBy && <ArrowDown className="inline p-1" />}
                 </div>
               </TableHead>
               <TableHead>
-                Created At
+                <Link href={{query: {...searchParams, orderBy: "createdAt"}}}>Created At</Link>
+                {"createdAt" === searchParams.orderBy && <ArrowDown className="inline p-1" />}
               </TableHead>
             </TableRow>
           </TableHeader>
